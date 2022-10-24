5f5f56e16e09f26c83e849045966df782a92a60a
donbing <ccbing@gmail.com>
2022-10-24 23:46:04 +0100

    pratchet quotes

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import os
+from queue import Empty
 import sys
 
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
@@ -44,24 +45,38 @@ class MyServer(tcp_sver.tcp_sver):
             #creat new Image and draw the image
             Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
             draw = ImageDraw.Draw(Himage)
-            draw.text((0, 0 ), 'Rusty Bee, Did a Wee', font = font24, fill = 0)
-            draw.text((0, 20), 'on the floor cos he was poor', font = font18, fill = 0)
-            draw.text((0, 40), 'C.Bingham', font = font18, fill = 0)    
-            #display
-            self.flush_buffer(epd.getbuffer(Himage))
-            #open image files
-            #Himage = Image.open(os.path.join(picdir, '4in2.bmp'))
-            #display
-            #self.flush_buffer(epd.getbuffer(Himage))
 
+            quote = get_pratchet_quote(1)
+            
+            for idx, x in enumerate(quote):
+                draw.text((0, idx * 15), x, font = font24, fill = 0)
+
+            self.flush_buffer(epd.getbuffer(Himage))
             self.Send_cmd('S')                    
         except ConnectionResetError :
             self.Wait_write("lose connect.")
         except KeyboardInterrupt :
             self.close()
             os.system("clear")
-        #except :
-        #    pass
+
+def get_pratchet_quote():
+    steps = 0
+    quotes_read = 0
+    target = 1
+    quote = []
+    with open('./pqf') as f:
+        for line in f:
+            if not line.strip():
+                steps += 1
+            else:
+                quote.add(line)
+            if steps == 2:
+                quotes_read += 1
+                steps = 0
+            if quotes_read == target:
+                break
+
+    return quote
             
         
 if __name__ == "__main__":
